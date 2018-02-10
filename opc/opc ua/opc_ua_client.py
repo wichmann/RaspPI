c843949164ce9dda4e994da341ace31dc39d49fb
Christian Wichmann <christian@freenono.org>
2018-02-10 18:50:02 +0100

    Improved script for accessing OPC UA server and added example certificate.

--- opc/opc ua/opc_ua_client.py
+++ opc/opc ua/opc_ua_client.py
@@ -6,35 +6,70 @@ Notwendige Bibliothek installieren:
 
     pip3 install opcua 
 
+Für den Zugriff auf Variablen wird die OPC UA Notation genutzt:
+
+    ns=<namespaceIndex>;<identifiertype>=<identifier>
+    
+    <namespace index> -> namespace index
+    <identifier type> -> flag that specifies the identifier type:
+                         Flag	Identifier Type
+                         i	    NUMERIC (UInteger)
+                         s	    STRING (String)
+                         g	    GUID (Guid)
+                         b	    OPAQUE (ByteString)
+
+Quelle: http://documentation.unified-automation.com/uasdkhp/1.0.0/html/_l2_ua_node_ids.html
+
 """
 
+from time import sleep
 from opcua import Client
 
 
-client = Client("opc.tcp://opcua.demo-this.com:51210/UA/SampleServer")
+client = Client("opc.tcp://192.168.10.132:4840")
+#client.set_security_string("Basic256,SignAndEncrypt,certificate.pem,privatekey.pem")
 
 try:
     client.connect()
     
-    # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+    # greife auf Elemente im Baum zu
     root = client.get_root_node()
-    print("Objects node is: ", root)
+    objects = client.get_objects_node()
+    app = objects.get_child(["2:DeviceSet", "4:CODESYS Control for Raspberry Pi SL", "4:Resources", "4:Application"])
     
-    # Node objects have methods to read and write node attributes as well as browse or populate address space
-    print("Children of root are: ", root.get_children())
+    # erzeuge Objekte für Knoten aus dem Baum über OPC UA Notation
+    eingang1 = client.get_node('ns=4;s=|var|CODESYS Control for Raspberry Pi SL.Application.PLC_PRG.eingang1')
+    eingang2 = client.get_node('ns=4;s=|var|CODESYS Control for Raspberry Pi SL.Application.PLC_PRG.eingang2')
+    ausgang = client.get_node('ns=4;s=|var|CODESYS Control for Raspberry Pi SL.Application.PLC_PRG.ausgang')
+    
+    # OPC-Variablen besitzen vier Attribute: Datentyp, Wert, Status, Zeitstempel
+    data = ausgang.get_data_value()
+    print('************ Variable: Ausgang ************')
+    print('Datentyp:    ', data.Value.VariantType)
+    print('Wert:        ', data.Value.Value)
+    print('Status:      ', data.StatusCode)
+    print('Zeitstempel: ', data.SourceTimestamp)
+    print('*******************************************')
+    
+    # Variable ausgang auslesen
+    print('Wert der Variable "ausgang": ', ausgang.get_value())
+    
+    # Variablen eingang1 und eingang2 schreiben
+    print('Setzen beider Eingänge auf True!')
+    eingang1.set_value(True)
+    eingang2.set_value(True)
+    sleep(1.0)
+    
+    # Variable ausgang auslesen
+    print('Wert der Variable "ausgang": ', ausgang.get_value())
+    
+    # Variablen eingang1 und eingang2 schreiben
+    print('Setzen eines Eingangs auf False!')
+    eingang2.set_value(False)
+    sleep(1.0)
+    
+    # Variable ausgang auslesen
+    print('Wert der Variable "ausgang": ', ausgang.get_value())
 
-    # get a specific node knowing its node id
-    #var = client.get_node(ua.NodeId(1002, 2))
-    #var = client.get_node("ns=3;i=2002")
-    #print(var)
-    #var.get_data_value() # get value of node as a DataValue object
-    #var.get_value() # get value of node as a python builtin
-    #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
-    #var.set_value(3.9) # set node value using implicit data type
-
-    # Now getting a variable node using its browse path
-    #myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
-    #obj = root.get_child(["0:Objects", "2:MyObject"])
-    #print("myvar is: ", myvar)
 finally:
     client.disconnect()
